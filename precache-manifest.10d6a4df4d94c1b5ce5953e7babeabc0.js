dec12166a198161ddf51e6d5e16706d53fb64e85
sergiy khurtik <skhurtik@ukr.net>
2020-02-12 03:12:46 +0200

    Updates

---
+++ precache-manifest.10d6a4df4d94c1b5ce5953e7babeabc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294c4059484ba585f2949b7c5db784ea",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "e281adc3603cf1037838",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.a1b60ed9.chunk.css"
+  },
+  {
+    "revision": "122a05d70fbf9d2c33a4",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.d6da8d00.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.d6da8d00.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e281adc3603cf1037838",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.e0cb25e3.chunk.js"
+  },
+  {
+    "revision": "20af662a8729a8db5d69",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.7aee43a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-phonebook/precache-manifest.3ee3ee75aa1181eb7eae8e4d96b28b38.js"
+  "/goit-react-hw-05-phonebook/precache-manifest.10d6a4df4d94c1b5ce5953e7babeabc0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-03-phonebook/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-05-phonebook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
